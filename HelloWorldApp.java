b1d9fd6d66c87b1cee3e87a6af5d9f8eecfab519
Dominic Carr <6775298+dominiccarr@users.noreply.github.com>
2022-11-29 16:18:22 +0000

    rendering java uncompilable

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,7 +1,7 @@
 public class HelloWorldApp {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hello World"
     System.out.println("Hello SQM class of 11 students!");
   }
 }
