016007cfab43dee4a83f2db4eed141fa5ed9568b
Dominic Carr <6775298+dominiccarr@users.noreply.github.com>
2023-11-30 15:31:50 +0000

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -4,5 +4,9 @@ public class HelloWorldApp {
     // edit
     System.out.println("Hello World");
     System.out.println("Hello SQM class of 11 students!");
+
+    if (Math.random() > 0.5){
+      System.exit(1);
+    }
   }
 }
